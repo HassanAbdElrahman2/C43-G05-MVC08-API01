b1ebe56fd9084d5086c82704d6fb446ca2f2b9b6
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-04-21 23:54:31 +0200

    Part 04 UnitOfWork

---
+++ Core/DomainLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using DomainLayer.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IUnitOfWork
+    {
+        public IGenericRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TKey : IEquatable<TKey>
+            where TEntity : BaseEntity<TKey>;
+        public Task CompleteAsync();
+    }
+}
---
+++ Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using DomainLayer.Common.Entities;
+using DomainLayer.Contracts;
+using Microsoft.IdentityModel.Tokens;
+using Persistence.Data;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.UnitOfWork
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly StoreDBContext _dBContext;
+        private readonly Dictionary<string,object> _repositories=[];
+        public UnitOfWork(StoreDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        public async Task CompleteAsync()
+        {
+           await _dBContext.SaveChangesAsync();
+        }
+
+        public IGenericRepository<TKey, TEntity> GetRepository<TKey, TEntity>()
+            where TKey : IEquatable<TKey>
+            where TEntity : BaseEntity<TKey>
+        {
+            // Get name Of Taype
+            var Type=typeof(TEntity).Name;
+            if(_repositories.ContainsKey(Type))
+                return (IGenericRepository<TKey,TEntity>) _repositories[Type];
+            else
+            {
+                var Rep= new GenericRepository<TKey,TEntity>(_dBContext);
+                _repositories.Add(Type, Rep);
+                return Rep;
+            }
+            
+        }
+    }
+}
