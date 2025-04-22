3d3138d97838d51ff9250ea4a763fc7da9fa5a93
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-04-22 04:47:43 +0200

    Part 06 Auto Mapper

---
+++ Core/ServiceImplementation/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceImplementation.MappingProfiles;
+
+namespace ServiceImplementation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddAutoMapper(typeof(ProductProfile).Assembly);
+
+            return services;
+        }
+    }
+}
---
+++ Core/ServiceImplementation/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using DomainLayer.Models.Products;
+using Shared.DataTranssferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceImplementation.MappingProfiles
+{
+    public class ProductProfile :Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDTO>()
+                .ForMember(dest=>dest.TypeName,option=>option.MapFrom(Scr=>Scr.ProductType.Name))
+                .ForMember(dest => dest.BrandName, option => option.MapFrom(Src => Src.ProductBrand.Name));
+            CreateMap<ProductType, TypeDTO>();
+            CreateMap<ProductBrand, BrandDTO>();
+
+        }
+    }
+}
