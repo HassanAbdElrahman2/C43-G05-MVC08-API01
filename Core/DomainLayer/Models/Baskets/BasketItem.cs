bffe22139b38b8b300532291794c1147bb7e8718
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-05-02 03:48:34 +0300

    Part 09 Basket Module - Entities

---
+++ Core/DomainLayer/Models/Baskets/BasketItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Baskets
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string PictureUrl  { get; set; }=default!;
+        public decimal price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Core/DomainLayer/Models/Baskets/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Baskets
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } // Created From Customer [FrontEnd]
+        public ICollection<BasketItem> Items { get; set; } = [];
+    }
+}
