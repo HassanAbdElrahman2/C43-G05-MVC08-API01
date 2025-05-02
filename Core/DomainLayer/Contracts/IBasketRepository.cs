b2f2662175c87075aad1095840d7ae2e8662985a
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-05-02 17:21:55 +0300

    Part 11 Basket Module - Repository

---
+++ Core/DomainLayer/Contracts/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using DomainLayer.Models.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket?>GetCustomerBasketAsync(string Key);
+        Task<CustomerBasket?> CreateOrUpdateBasketAsync(CustomerBasket basket, TimeSpan? time = null);
+        Task<bool>DeleteBasketAsync(string id);
+    }
+}
