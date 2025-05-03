2eeae6722a4dbdcf9dc974f3ce1f11d829352128
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-05-03 16:40:50 +0300

    Part 10 Authentication Controller [Address , Email And Get User ]

--- Infrastructure/Presentation/Controllers/AuthenticationController.cs
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
 using Shared.IdentityDto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +19,34 @@ namespace Presentation.Controllers
         [HttpPost("Register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         => Ok(await _serviceManager.AuthenticationService.RegisterAsync(registerDto));
+        [HttpGet("CheckEmail")]
+        public async Task<ActionResult<bool>> CheckEmail(string Email)
+            => Ok(await _serviceManager.AuthenticationService.CheckEmailAsync(Email));
+        [Authorize]
+        [HttpGet("CurrentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            return Ok(await _serviceManager.AuthenticationService.CheckEmailAsync(email!));
+        }
+        [Authorize]
+        [HttpGet("Address")]
+        public async Task<ActionResult<AddressDto>> GetCurrentUserAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            return Ok(await _serviceManager.AuthenticationService.GetCurrentUserAddressAsync(email!));
+        }
+        [Authorize]
+        [HttpPut("Address")]
+        public async Task<ActionResult<AddressDto>> UpdateCurrentUserAddress(AddressDto addressDto)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            return Ok(await _serviceManager.AuthenticationService.UpdateCurrentUserAddressAsync(email,addressDto));
+        }
+
+
+
+
 
     }
 }
