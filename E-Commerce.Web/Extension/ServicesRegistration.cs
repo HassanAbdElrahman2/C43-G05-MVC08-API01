222e4a1807bb7c05bc438b2a25c489e04b935d3c
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-05-03 14:43:54 +0300

    Part 08 Jwt Options & Token Validation

--- E-Commerce.Web/Extension/ServicesRegistration.cs
+++ E-Commerce.Web/Extension/ServicesRegistration.cs
@@ -1,5 +1,8 @@
 ﻿using E_Commerce.Web.Factories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace E_Commerce.Web.Extension
 {
@@ -18,6 +21,29 @@ namespace E_Commerce.Web.Extension
             {
                 Options.InvalidModelStateResponseFactory = ApiResponseFactory.GenerateApiValidationErrorResponse;
 
+            });
+            return Services;
+        }
+        public static IServiceCollection AddJWTService(this IServiceCollection Services, IConfiguration configuration)
+        {
+            Services.AddAuthentication(Config =>
+            {
+                Config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                Config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(Options =>
+            {
+                Options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JWTOptions:Issuer"],
+
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWTOptions:Audience"],
+
+
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTOptions:SecurityKey"]))
+                };
             });
             return Services;
         }
--- Infrastructure/Presentation/Controllers/ProductController.cs
+++ Infrastructure/Presentation/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
 using Shared;
 using Shared.DataTranssferObjects;
@@ -13,6 +14,7 @@ namespace Presentation.Controllers
    
     public class ProductController(IServiceManager _serviceManager) : ApiBaseController
     {
+        [Authorize("Admin")]
         [HttpGet]
         public async Task<ActionResult<PaginatedResult<ProductDTO>>> GetAllProdcuts([FromQuery]ProductQueryPrams productQueryPrams)
         {
