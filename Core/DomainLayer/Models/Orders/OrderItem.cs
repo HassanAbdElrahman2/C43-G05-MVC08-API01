ea1d3664fcf77f485dd3a0d83c69a5e83e5b300d
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-05-17 19:09:55 +0300

    Part 02 Order Module Entities

---
+++ Core/DomainLayer/Models/Orders/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Orders
+{
+    public class OrderItem : BaseEntity<int>
+    {
+        ProductItemOrder Product { get; set; } = default!;
+        public decimal price  { get; set; }
+        public int Quantity{ get; set; }
+
+    }
+}
