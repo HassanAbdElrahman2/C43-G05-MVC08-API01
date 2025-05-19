67647b6b9c4f036ea2e4b37797a64859eb49a8d4
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-05-20 00:04:02 +0300

    Part 05 Order Profile [ Mapping ]

---
+++ Core/ServiceImplementation/MappingProfiles/OrderItemPictureUrlResolver.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DomainLayer.Models.Orders;
+using Microsoft.Extensions.Configuration;
+using Shared.OrderDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceImplementation.MappingProfiles
+{
+    internal class OrderItemPictureUrlResolver(IConfiguration _configuration) : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+        {
+            if (source.Product.PictureUrl is null)
+                return String.Empty;
+            else
+            {
+                var Image = $"{_configuration.GetSection("Urls")["BaseUrl"]}/{source.Product.PictureUrl}";
+                return Image;
+            }
+        }
+    }
+}
--- Core/ServiceImplementation/MappingProfiles/OrderProfile.cs
+++ Core/ServiceImplementation/MappingProfiles/OrderProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DomainLayer.Models.Orders;
 using Shared.IdentityDto;
+using Shared.OrderDto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,13 @@ namespace ServiceImplementation.MappingProfiles
     {
         public OrderProfile()
         {
-            CreateMap<AddressDto, OrderAddress>();
+            CreateMap<AddressDto, OrderAddress>().ReverseMap();
+
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(S => S.DeliveryMethod, O => O.MapFrom(D => D.DeliveryMethod.ShortName));
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(S => S.ProductName, O => O.MapFrom(D => D.Product.ProductName))
+                .ForMember(S=>S.PictureUrl,O=>O.MapFrom<OrderItemPictureUrlResolver>());
         }
     }
 }
